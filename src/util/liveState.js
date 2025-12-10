563fd13240036d83fd95d37395b608a6ef5767d8
Niels Nielsen <niels.nielsen@gmail.com>
2025-12-10 15:50:58 -0700

    state defaults to an empty object

--- src/util/liveState.js
+++ src/util/liveState.js
@@ -1,18 +1,17 @@
 import { getCurrentRoute } from "./url.js"
 
 // each page gets its own state object
-const states = new Map()
+const pageStates = new Map()
 
 // make state object globally available for debugging
-window.__LIVE_STATES = states
+window.__LIVE_STATES = pageStates
 
 // make liveState a globally available function that can be called from live-pages
-window.liveState = (defaultState) => {
-  if (defaultState == null) { return }
+window.liveState = (defaultState = {}) => {
   const currentPage = getCurrentRoute()
-  const currentState = states.get(currentPage)
+  const currentState = pageStates.get(currentPage)
   if (!currentState) {
-    states.set(currentPage, defaultState)
+    pageStates.set(currentPage, defaultState)
     return defaultState
   }
   return currentState
